99ec38dc17fea3bc606b7b3919d63c61fe45f925
amarnath <avadla@gemini-us.com>
2019-09-30 13:05:09 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const mongoDbUri = require('./config.js').localUri
+
+mongoose.connect(mongoDbUri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then((data) => {
+    console.log("connected to mongodb : " + mongoDbUri)
+}).catch((err) => {
+    console.log(err);
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log("listening to " + port);
+})
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoUri : "mongodb+srv://amar:amar@shopping-qsj78.mongodb.net/test?retryWrites=true&w=majority",
+    localUri : "mongodb://localhost:27017/milieu"
+}
