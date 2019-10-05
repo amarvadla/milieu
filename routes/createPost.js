e1b1255a6f22c0c4b1a48931ea057878b8b5a26e
amarv <amarvadla27@gmail.com>
2019-10-05 16:32:02 +0530

    user posts and likes

--- routes/createPost.js
+++ routes/createPost.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userSchema = require('../model/userProp')
 const postSchema = require('../model/post')
+const ObjectId = require('mongoose').Types.ObjectId
 
 router.post('/createPost', (req, res) => {
 
@@ -74,38 +75,114 @@ router.post('/like', (req, res) => {
     var input = req.body
 
     getUser(input.userId).then((data) => {
-        if (data && input.likeStatus == 1) {
-            postSchema.update({
-                _id: input.postId
-            }, { $push: { likes: { userId: input.userId } } }, (err, postData) => {
-                if (postData) {
-                    res.send({
-                        statusCode: 1,
-                        statusMessage: 'successfully liked',
-                        data: {
-                            id: postData._id
-                        }
-                    })
+
+        likeOrNot(input.postId, input.userId).then((result) => {
+
+
+            if (!result[0].likeStatus) {
+                postSchema.update({
+                    _id: input.postId
+                }, { $push: { likes: { userId: input.userId } } }, (err, postData) => {
+                    if (postData) {
+                        res.send({
+                            statusCode: 1,
+                            statusMessage: 'successfully liked',
+                            data: {
+                                id: postData._id
+                            }
+                        })
+                    }
+                })
+            } else {
+                postSchema.update({
+                    _id: input.postId
+                }, { $pull: { likes: { userId: input.userId } } }, (err, postData) => {
+                    if (postData) {
+                        res.send({
+                            statusCode: 1,
+                            statusMessage: 'successfully unliked',
+                            data: {
+                                id: postData._id
+                            }
+                        })
+                    }
+                })
+            }
+
+        })
+    }).catch((e) => {
+        res.send({
+            statusCode: 0,
+            statusMessage: e
+        })
+    })
+})
+
+
+function likeOrNot(postId, userId) {
+    return new Promise((resolve, reject) => {
+        postSchema.aggregate([
+            { $match: { '_id': ObjectId(postId) } },
+            {
+                $project: {
+                    'likeStatus': {
+                        $in: [ObjectId(userId), '$likes.userId']
+                    }
                 }
-            })
-        } else if (data && input.likeStatus == 0) {
-            postSchema.update({
-                _id: input.postId
-            }, { $pull: { likes: { userId: input.userId } } }, (err, postData) => {
-                if (postData) {
-                    res.send({
-                        statusCode: 1,
-                        statusMessage: 'successfully unliked',
-                        data: {
-                            id: postData._id
-                        }
-                    })
+            }
+        ]).exec((err, result) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+router.get('/getUserPosts', (req,res) => {
+
+    var input = req.query
+
+    getUser(input.userId).then((data) => {
+        if(data){
+
+            var postArray = []
+
+            postSchema.find({ userId : input.userId} , (err,data) => {
+
+                for(var i=0;i<data.length;i++){
+
+                    var post = {}
+
+                    post.id = data[i]._id
+                    post.createdDate = data[i].createdDate
+                    post.comments = data[i].comments.length
+                    post.likes = data[i].likes.length
+                    post.postType = data[i].postType
+                    post.sourceUrl = data[i].sourceUrl
+                    post.postText = data[i].postText
+
+                    postArray.push(post)
+
                 }
+
+                res.send({
+                    statusCode : 1,
+                    statusMessage: 'success',
+                    data : postArray
+                })
+
             })
+
         }
     }).catch((e) => {
-
+        res.send({
+            statusCode : 1,
+            statusMessage: e
+        })
     })
+
 })
 
 module.exports = router
