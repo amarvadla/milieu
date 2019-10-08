89a1b29ccb12229243956cb887301b730807ab6a
amarv <amarvadla27@gmail.com>
2019-10-08 14:43:50 +0530

    add friends and reject requests

--- routes/sendFriendRequest.js
+++ routes/sendFriendRequest.js
@@ -23,6 +23,128 @@ router.post('/sendRequest', (req, res) => {
 
 })
 
+router.get('/getFriendRequests', (req, res) => {
+
+    var input = req.query
+
+    getUser(input.userId).then((data) => {
+
+        userSetting.findOne({ ownerId: input.userId }, (err, result) => {
+
+            if (result) {
+
+                var array = []
+
+                result.friendRequests.forEach((element) => {
+                    var obj = {}
+
+                    obj.id = element._id
+                    obj.userId = element.userId
+                    obj.date = element.date
+                    obj.blocked = element.blocked
+
+                    array.push(obj)
+                })
+
+                res.send({
+                    statusCode: 1,
+                    statusMessage: 'success',
+                    data: array
+                })
+            } else if (err) {
+                res.send(err)
+            }
+        })
+    }).catch(e => res.send(e))
+
+
+})
+
+
+router.post('/respondFriendRequest', (req, res) => {
+
+    var input = req.body
+
+    getUser(input.userId).then((data) => {
+
+        if (data) {
+
+            userSetting.findOne({ ownerId: input.userId }, (err, result) => {
+
+                if (result) {
+
+                    if (input.accepted) {
+                        Promise.all([removeFromrequestList(input), addTofreindsList(input)]).then((promiseData) => {
+
+                            if (promiseData.length == 2) {
+                                res.send({
+                                    statusCode: 1,
+                                    statusMessage: 'successfully added to freindsList'
+                                })
+                            }
+
+                        }).catch((e) => {
+                            res.send(e)
+                        })
+                    } else {
+                        removeFromrequestList(input).then((data) => {
+                            res.send({
+                                statusCode: 1,
+                                statusMessage: 'deleted from requestList'
+                            })
+                        })
+                    }
+
+                } else {
+                    res.send({
+                        statusCode: 0,
+                        statusMessage: 'create your profile to accept'
+                    })
+                }
+
+            })
+
+        }
+
+    }).catch(e => res.send(e))
+
+})
+
+function removeFromrequestList(input) {
+
+    return new Promise((resolve, reject) => {
+
+        userSetting.update({ ownerId: input.userId },
+            { $pull: { friendRequests: { userId: input.friendId } } }, (err, result) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(result)
+                }
+            }
+        )
+
+    })
+
+}
+
+function addTofreindsList(input) {
+
+    return new Promise((resolve, reject) => {
+
+        userSetting.update({ ownerId: input.userId },
+            { $push: { freindsList: { userId: input.friendId } } }, (err, result) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(result)
+                }
+            }
+        )
+
+    })
+}
+
 function saveFriendRequest(userId, friendId, res) {
 
     if (userId.equals(friendId)) {
@@ -71,22 +193,32 @@ function isRequestSent(userId, friendId) {
         userSetting.findOne({ ownerId: userId }, (err, data) => {
             if (data) {
                 var friendRequests = data.friendRequests
+                var freindsList = data.freindsList
 
                 var alreadySent = false
+                var message = ''
 
                 friendRequests.forEach((element) => {
                     if (element.userId.equals(friendId)) {
                         alreadySent = true
+                        message = 'request already sent'
+                    }
+                })
+
+                freindsList.forEach((element) => {
+                    if (element.userId.equals(friendId)) {
+                        alreadySent = true
+                        message = 'already in you friends list'
                     }
                 })
 
                 // var boolArray = friendRequests.map(obj => obj.userId.equals(friendId))
 
-                console.log(alreadySent);
+                // console.log(alreadySent);
 
 
                 if (alreadySent) {
-                    reject('request already sent')
+                    reject(message)
                 } else {
                     resolve(true)
                 }
