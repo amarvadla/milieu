9776a83b44f3eb73c859653f7320789ff55b34be
amarv <amarvadla27@gmail.com>
2019-10-21 15:00:13 +0530

    port 5000

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ mongoose.connect(mongoDbUri, {
     console.log(err);
 })
 
-const port = process.env.PORT || 5000
+const port = 5000
 
 app.listen(port, () => {
     console.log("listening to " + port);
