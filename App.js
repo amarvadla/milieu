bd7284afde61fad8fa8fc8e1c8079acc0bee6e0b
amarnath <avadla@gemini-us.com>
2019-10-28 12:20:51 +0530

    app.js

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ app.use('/homeFeeds', require('./routes/homeFeeds'))
 app.get('*', (req, res) => res.send('Page Not found 404'));
 app.post('*', (req, res) => res.send('not available'));
 
-const mongoDbUri = require('./config.js').mongoUri
+const mongoDbUri = require('./config.js').localUri
 
 mongoose.connect(mongoDbUri, {
     useUnifiedTopology: true,
